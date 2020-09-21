9018393168ab4762abe621c3c13e0f2610deed05
정성훈 <52942411+jsh0128@users.noreply.github.com>
2020-09-21 20:28:21 +0900

    [UPDATE] get city send succes

--- back-end/testapp/controller/city.js
+++ back-end/testapp/controller/city.js
@@ -6,68 +6,54 @@ const request = require("request");
 const { map } = require("../app");
 var parseString = require("xml2js").parseString;
 
-let cityData = "";
+const getCity = (req, res) => {
+  var url =
+    "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson";
+  var queryParams =
+    "?" + encodeURIComponent("ServiceKey") + api_key; /* Service Key*/
+  queryParams +=
+    "&" +
+    encodeURIComponent("ServiceKey") +
+    "=" +
+    encodeURIComponent("-"); /* */
+  queryParams +=
+    "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1"); /* */
+  queryParams +=
+    "&" +
+    encodeURIComponent("numOfRows") +
+    "=" +
+    encodeURIComponent("10"); /* */
+  queryParams +=
+    "&" +
+    encodeURIComponent("startCreateDt") +
+    "=" +
+    encodeURIComponent("20200410"); /* */
+  queryParams +=
+    "&" +
+    encodeURIComponent("endCreateDt") +
+    "=" +
+    encodeURIComponent("20200410"); /* */
 
-let getCity = (req, res) => {};
+  url = url + queryParams;
 
-var url =
-  "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson";
-var queryParams =
-  "?" + encodeURIComponent("ServiceKey") + api_key; /* Service Key*/
-queryParams +=
-  "&" + encodeURIComponent("ServiceKey") + "=" + encodeURIComponent("-"); /* */
-queryParams +=
-  "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1"); /* */
-queryParams +=
-  "&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("10"); /* */
-queryParams +=
-  "&" +
-  encodeURIComponent("startCreateDt") +
-  "=" +
-  encodeURIComponent("20200410"); /* */
-queryParams +=
-  "&" +
-  encodeURIComponent("endCreateDt") +
-  "=" +
-  encodeURIComponent("20200410"); /* */
+  request.get(url, (err, response, body) => {
+    if (err) {
+      console.log(`err => ${err}`);
+    } else {
+      if (response.statusCode == 200) {
+        var result = body;
+        // console.log(`body data => ${result}`);
 
-// request(
-//   {
-//     url: url + queryParams,
-//     method: "GET",
-//   },
-//   (getCity = (error, response, body) => {
-//     // console.log("Status", response.statusCode);
-//     // console.log("Headers", JSON.stringify(response.headers));
-//     // console.log("Reponse received", body);
-
-//     // console.log("asdfdf");
-
-//     parseString(body, function (err, result) {
-//       // console.log(result);
-//       cityData = result.response.body[0].items[0];
-//     });
-
-//     message: cityData;
-//   })
-// );
-
-// module.exports = {
-//   getCity: getCity,
-// };
-url = url + queryParams;
-console.log(url);
-request.get(url, (err, res, body) => {
-  if (err) {
-    console.log(`err => ${err}`);
-  } else {
-    if (res.statusCode == 200) {
-      var result = body;
-      console.log(`body data => ${result}`);
-
-      var xmlToJson = convert.xml2json(result, { compact: true, spaces: 4 });
-
-      console.log(`xml to json => ${xmlToJson}`);
+        var xmlToJson = convert.xml2json(result);
+        res.status(200).json({
+          message: xmlToJson,
+        });
+        // console.log(`xml to json => ${xmlToJson}`);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = {
+  getCity: getCity,
+};
