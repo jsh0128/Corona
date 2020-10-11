569a8561c01a26c6a1be71a53c2a498937158ba4
정성훈 <52942411+jsh0128@users.noreply.github.com>
2020-10-11 21:45:59 +0900

    [UPDATE] 항목 저장 성공

--- front-end/src/containers/City/CityContainer.js
+++ front-end/src/containers/City/CityContainer.js
@@ -1,34 +1,85 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import City from "components/City";
 import axios from "axios";
 import GetApi from "../../assets/api/GetApi";
+// 확진자수 2 지역 3 전일대배 증가수 3 사망자 수 1
+
+const CITY_COUNT = 19;
+let newAll = {};
+let all = []
 
 const CityContainer = () => {
+  const [newInformation, setNewInformation] = useState({
+    area: "",
+    check: 0,
+    increase: 0,
+    death: 0,
+  });
+  const [api, setApi] = useState(true);
+  const [idx, setIdx] = useState(0);
+  const [informatin, setInformation] = useState([]);
+  const [area, setArea] = useState("");
+  const [check, setCheck] = useState(0);
+  const [increase, setIncrease] = useState(0);
+  const [death, setDeath] = useState(0);
+
   const getApi = async () => {
     const data = await GetApi.getCity();
     return data.elements[0].elements[1].elements[0];
   };
+
+  const cityInformation = (response) => {
+    if (idx < 1) {
+      for (let i = 0; i < CITY_COUNT; i++) {
+        setArea(response.elements[i].elements[3].elements[0].text);
+        setCheck(response.elements[i].elements[2].elements[0].text);
+        setIncrease(response.elements[i].elements[6].elements[0].text);
+        setDeath(response.elements[i].elements[1].elements[0].text);
+        setNewInformation({
+          area: area,
+          check: check,
+          increase: increase,
+          death: death,
+        });
+        if (idx === 0) {
+          setInformation({});
+          setIdx(idx + 1);
+        } else {
+          setInformation({ ...informatin, idx: newInformation });
+          setIdx(idx + 1);
+        }
+      }
+    }
+  };
+
   useEffect(() => {
     getApi().then((response) => {
-      // 확진자수 2 지역 3 전일대배 증가수 3 사망자 수 1
+      // cityInformation(response);
       for (let i = 0; i < 19; i++) {
-        // console.log(response.elements[i]);
-        console.log(
-          " 지역 : " + response.elements[i].elements[3].elements[0].text
-        );
-        console.log(
-          " 확진자수 : " + response.elements[i].elements[2].elements[0].text
-        );
-        console.log(
-          " 전일대비 증가수: " +
-            response.elements[i].elements[6].elements[0].text
-        );
-        console.log(
-          " 사망자 수 : " + response.elements[i].elements[1].elements[0].text
-        );
+       let data = {
+          area: response.elements[i].elements[3].elements[0].text,
+          check: response.elements[i].elements[2].elements[0].text,
+          update: response.elements[i].elements[6].elements[0].text,
+          death: response.elements[i].elements[1].elements[0].text,
+        }
+        all =[  ...all, data ]
       }
+      console.log(all);
     });
   }, []);
+
+  // useEffect(() => {
+  //   console.log("나");
+  //   if (api === true) {
+  //     console.log("다");
+  //     setApi(false);
+  //   } else {
+  //     console.log("라");
+
+  //     // cityInformation();
+  //   }
+  // }, [area, check, increase, death]);
+
   return (
     <div>
       <City getApi={getApi} />
