8886683cd1c22ff769cc513bdcf43527beb52a79
정성훈 <52942411+jsh0128@users.noreply.github.com>
2020-11-30 13:56:30 +0900

    [UPDATE] remove front-end file

--- front-end/README.md
+++
@@ -1,9 +0,0 @@
-## create-react-app-mobx
-
-🛠 create-react-app with mobx
-
-### Install
-
-```bash
-$ git clone https://github.com/Sh031224/create-react-app-mobx.git
-```
--- front-end/src/pages/HostpialPage.js
+++
@@ -1,8 +0,0 @@
-import HostipialContainer from "containers/Hostipial/HostipialContainer"
-import React from "react"
-
-const HostpialPage = () => {
-  return <HostipialContainer />
-}
-
-export default HostpialPage
