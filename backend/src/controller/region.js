8041976b32afd18c6e2a1932f209a5868399a94e
정성훈 <52942411+jsh0128@users.noreply.github.com>
2020-12-02 09:47:03 +0900

    [FIX] back-end region.js send params

--- backend/src/controller/region.js
+++ backend/src/controller/region.js
@@ -2,6 +2,8 @@ const { city_api } = require("../../config/config.json")
 const xmlToJsonConverter = require("xml-js")
 const axios = require("axios")
 const { formatYYYYMMDD } = require("../util/dateUtil")
+const fs = require("fs")
+const path = require("path")
 
 const GET_REGION_URL =
   "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson"
@@ -11,7 +13,7 @@ module.exports = async (req, res) => {
 
   let formatedDate = formatYYYYMMDD(now)
 
-  const body = {
+  const params = {
     pageNo: 1,
     numOfRows: 10,
     startCreateDt: "20200410",
@@ -20,15 +22,13 @@ module.exports = async (req, res) => {
   const url = `${GET_REGION_URL}?ServiceKey=${city_api}`
 
   try {
-    console.log(url)
-    const response = await axios.get(url, body)
+    const response = await axios.get(url, { params: params })
     const { data } = response
 
     res.status(200).json({
       data,
     })
   } catch (err) {
-    console.log(err)
     res.status(500).json({
       message: "서버 오류",
     })
